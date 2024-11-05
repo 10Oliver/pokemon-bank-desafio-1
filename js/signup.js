4b1ae75196431e15cb6a8c7ed10c2cf99655eedf
10Oliver <61370001+10Oliver@users.noreply.github.com>
2024-11-05 17:01:57 +0000

    🛂 ♿️ Improve stregth password indicator

--- js/signup.js
+++ js/signup.js
@@ -15,8 +15,8 @@ document.getElementById("register-btn").addEventListener("click", () => {
         return;
     }
     if (!password.value) {
-        alert("La contraseña es requerida");     
-        return;   
+        alert("La contraseña es requerida");
+        return;
     }
 
     if (password.value != confirmPassword.value) {
@@ -34,7 +34,7 @@ document.getElementById("register-btn").addEventListener("click", () => {
 
     alert("Usuario creado correctamente");
     location.href = "../index.html";
-}); 
+});
 
 document.getElementById("show-password").addEventListener("click", () => {
     document.getElementById("hide-password").classList.remove("d-none");
@@ -63,3 +63,62 @@ document.getElementById("hide-confirm-password").addEventListener("click", () =>
     // Change input type
     document.getElementById("confirm-password-input").type = "password";
 });
+
+/**
+ * Password security indicator
+ */
+document.getElementById("password-input").addEventListener("keyup", () => {
+    let score = 4;
+    let color = "#ff6b6b";
+    const password = document.getElementById("password-input").value;
+
+    if (password.length > 0) {
+        const lengthScore = Math.min(password.length / 8, 1) * 20;
+        score += lengthScore;
+    }
+
+    if (/[a-z]/.test(password)) {
+        score += 19;
+    }
+
+    if (/[A-Z]/.test(password)) {
+        score += 19;
+    }
+
+    if (/[0-9]/.test(password)) {
+        score += 19;
+    }
+
+    if (/[^a-zA-Z0-9]/.test(password)) {
+        score += 19;
+    }
+
+    if (score > 20 && score <= 40) {
+        color = "#ff9e5d";
+    }
+
+    if (score > 40 && score <= 60) {
+        color = "#ffc94d";
+    }
+
+    if (score > 60 && score <= 80) {
+        color = "#ffeb3b";
+    }
+
+    if (score > 80 && score <= 99) {
+        color = "#b6d94c";
+    }
+
+    if (score == 100) {
+        color = "#10bd17"
+    }
+
+    const bar = document.getElementById("password-bar");
+    bar.style.width = `${score}%`;
+    bar.style.backgroundColor = color;
+});
+
+document.getElementById("password-input").addEventListener("focus", () => {
+    const container = document.getElementById("password-bar-container");
+    container.classList.add("visible")
+})

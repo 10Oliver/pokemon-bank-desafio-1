bb09ce839c455bfb982e2a52fbd1dc9efab5de93
Melissa <vmelissalp@gmail.com>
2024-11-05 19:12:02 -0600

    cambios esteticos en Home

--- js/home.js
+++ js/home.js
@@ -1,4 +1,14 @@
 
+fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+    });
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => { document.getElementById('footer-placeholder').innerHTML = data; });
+
 
 document.getElementById("logout-btn").addEventListener("click", () => {
     logout();
